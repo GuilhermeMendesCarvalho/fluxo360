d17b16afa33e2eab2c9430b57ba68a71b441c0f6
Guilherme Mendes Carvalho <84997623+GuilhermeMendesCarvalho@users.noreply.github.com>
2025-05-01 17:00:46 -0300

    estrutura inicial com configurações feitas

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+// eslint.config.js
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: { react },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
